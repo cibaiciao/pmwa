45024c279c219720d685024836176fb35e9ed4a0
Tri Nguyen <cibaiciao@yahoo.com>
2015-04-04 15:49:57 -0700

    filter out for tasks

--- application/models/api_model.php
+++ application/models/api_model.php
@@ -253,7 +253,7 @@ class Api_model extends CI_Model {
                         $this->db->where('COALESCE(assignee,0)',$value);
                         break;
                     case 'unresolved':
-                        if ( $value ) {
+                        if ( $value && $key != 'status' ) {
                             $this->db->where_in('status',array(0,1));
                         }
                         break;
