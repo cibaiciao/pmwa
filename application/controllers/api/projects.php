4b26ef1b680efb5ad7513f3db4471d494bcf91ad
Tri Nguyen <cibaiciao@yahoo.com>
2015-04-04 15:22:10 -0700

    filter out for tasks

--- application/controllers/api/projects.php
+++ application/controllers/api/projects.php
@@ -246,7 +246,7 @@ class Projects extends MY_RestController
                     foreach ( $data as &$_data ) {
                         $percent = $totalUnResolved > 0 && $_data['issues'] > 0 ? number_format($_data['issues'] / $totalUnResolved * 100) : 0;
                         $html[] = "<tr>";
-                        $html[] = "<td><a href='#'>".$this->Api_model->getCreatedBy($_data['assignee'])."</a></td>";
+                        $html[] = "<td><a href='".$link."?assignee=".$_data['assignee']."&unresolved=1'>".$this->Api_model->getCreatedBy($_data['assignee'])."</a></td>";
                         $html[] = "<td>".$_data['issues']."</td>";
                         $html[] = "<td><div class='progress'><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"{$percent}\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: {$percent}%;\">".$percent."%</div></div></td>";
                         $html[] = "</tr>";
